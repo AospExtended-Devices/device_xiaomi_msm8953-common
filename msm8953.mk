8aef65e4e8afc12da68d81003793116a54a48e5c
MASTERGUY <murtipipo@gmail.com>
2020-05-02 16:21:33 +0530

    msm8953-common: Exclude RRO Overlays for snap camera

--- msm8953.mk
+++ msm8953.mk
@@ -316,6 +316,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+#RRO
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Snap
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
